2e83a74fadcf5b43e13a45c25488d60210e8e5cc
Ericka Sánchez Valencia <erickasanchez@ciencias.unam.mx>
2023-04-28 23:36:43 -0600

    push

---
+++ Mis proyectos 2023-2/aproximacion_de_pi backup 2.jl
@@ -0,0 +1,15 @@
+### A Pluto.jl notebook ###
+# v0.19.22
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ a5bf6180-e2be-11ed-0575-a99a6caafbd3
+using Plots, PlutoUI,StatPlots
+
+# ╔═╡ 25a9f2a5-bf64-49ef-ac19-284b0414aeaa
+fig = Figure(resolution =(3840,20160))
+
+# ╔═╡ Cell order:
+# ╠═a5bf6180-e2be-11ed-0575-a99a6caafbd3
+# ╠═25a9f2a5-bf64-49ef-ac19-284b0414aeaa
